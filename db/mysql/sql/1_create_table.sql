9c39265349679ce10c8a5ecb66c4ec80d3ebc9a5
mahiro72 <mahiro.yoshie@gmail.com>
2022-09-22 01:32:31 +0900

    title change

--- db/mysql/sql/1_create_table.sql
+++ db/mysql/sql/1_create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `tasks` (
   `id`     int(11) COLLATE utf8mb4_bin NOT NULL COMMENT 'task id',
-  `title`  varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'taskのtitle',
+  `name`  varchar(255) COLLATE utf8mb4_bin NOT NULL COMMENT 'taskの名前',
   `done`   boolean COLLATE utf8mb4_bin NOT NULL COMMENT 'taskが完了したかどうか',
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='taskを管理するtable';
--- db/mysql/sql/2_insert_dummy_data.sql
+++ db/mysql/sql/2_insert_dummy_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO tasks (id, title, done) VALUES (1, "task1", true);
-INSERT INTO tasks (id, title, done) VALUES (2, "task2", true);
-INSERT INTO tasks (id, title, done) VALUES (3, "task3", true);
-INSERT INTO tasks (id, title, done) VALUES (4, "task4", false);
-INSERT INTO tasks (id, title, done) VALUES (5, "task5", false);
+INSERT INTO tasks (`id`, `name`, `done`) VALUES (1, "task1", true);
+INSERT INTO tasks (`id`, `name`, `done`) VALUES (2, "task2", true);
+INSERT INTO tasks (`id`, `name`, `done`) VALUES (3, "task3", true);
+INSERT INTO tasks (`id`, `name`, `done`) VALUES (4, "task4", false);
+INSERT INTO tasks (`id`, `name`, `done`) VALUES (5, "task5", false);
