1458fb31c3baed224df71e550d9b5c6f29a5c9d5
mahiro72 <mahiro.yoshie@gmail.com>
2022-10-02 23:05:27 +0900

    add health comment

--- src/web/router/health.go
+++ src/web/router/health.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Healthはサーバーのヘルスチェックをするハンドラーです
 func (r Router) Health() {
 	r.engine.GET("/health", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"health": "good"})
